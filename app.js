db3dcc559df7841ea3999a5007eb7ac302f405b4
Mohit Urkude <mohiturkude8237@gmail.com>
2026-02-17 21:19:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Kubernetes Phase-3 🚀");
+  // res.end("Hello from Kubernetes Phase-3 🚀");
+  res.end("Version 2 🚀");
+
 });
 
 server.listen(3000);
